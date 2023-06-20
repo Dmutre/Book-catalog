b7e38275a426915fab93f26bff59d0a9d4f82460
Dmutre <dmytro.lesko@gmail.com>
2023-06-20 17:10:46 +0200

    0.0.2
    Setup for connection to mongoDB, added .env file, that is in gitignore. Connection works

--- server.js
+++ server.js
@@ -1,17 +1,27 @@
 "use strict";
 
+if(process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
 const express = require("express");
 const expressLayouts = require("express-ejs-layouts");
-const path = require("path");
-const indexRouter = require("./router/index.js")
+const mongoose = require("mongoose");
+const indexRouter = require("./router/index.js");
 
 const app = express();
 
 app.set("view engine", "ejs");
-//app.set('views', path.join(__dirname + 'views'));
-app.set('layout', 'layout/layout');
+app.set("layout", "layout/layout");
 app.use(expressLayouts);
 app.use(express.static("public"));
+
+mongoose.connect(process.env.DATABASE_URL, {
+  useNewUrlParser: true
+})
+const db = mongoose.connection;
+db.on("error", error => console.error(error));
+db.once("open", () => console.log("Connected to mongoose"));
 
 app.use("/", indexRouter);
 
