ab0c023a88423c3272f0064a9526dbe41ff47437
Dmutre <dmytro.lesko@gmail.com>
2023-06-21 21:10:25 +0200

    0.0.6.1
    Completed form built

--- router/books.js
+++ router/books.js
@@ -14,7 +14,6 @@ router.get("/", async (req, res) => {
 router.get("/new", async (req, res) => {
   try {
     const authors = await Author.find();
-    console.log(authors);
     const book = new Book();
     res.render("books/new", {
       authors: authors,
